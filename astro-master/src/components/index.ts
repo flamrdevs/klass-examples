6a41610ed06b8844d7b3ae0dd9cce1704797c80f
flamrdevs <flamrdevs@gmail.com>
2023-11-17 03:04:27 +0700

    chore: remove all examples

--- astro-master/src/components/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as Box } from "./Box.astro";
-export { default as Button } from "./Button.astro";
-export { default as HTML } from "./HTML.astro";
-export { default as IconButton } from "./IconButton.astro";
-export { default as LucideGithubIcon } from "./LucideGithubIcon.astro";
-export { default as LucideMoonIcon } from "./LucideMoonIcon.astro";
-export { default as LucideSunIcon } from "./LucideSunIcon.astro";
--- qwik-tailwind/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import "@builder.io/qwik/qwikloader.js";
-
-import { render } from "@builder.io/qwik";
-
-import { App } from "~/app.tsx";
-
-import "~/globals.css";
-
-render(document.getElementById("app") as HTMLElement, <App />);
